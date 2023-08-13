a3b434b087697b4a06e507b90a5ed78b3a4326db
Koki Takahashi <hakatasiloving@gmail.com>
2023-08-13 22:00:42 +0000

    Implement image tags inference

--- worker/main.py
+++ worker/main.py
@@ -1,23 +1,73 @@
-from firebase_functions import https_fn
+from firebase_functions import https_fn, options
 from firebase_admin import initialize_app, storage
 from io import BytesIO
 from joblib import load
-from deepdanbooru import pretrained_resnet50_model
+import torch
+from torchvision import models
+from danbooru_resnet import _resnet
+from tagger import get_raw_tags
+from PIL import Image
+import json
 
 initialize_app()
 
-@https_fn.on_request()
-def executeTestFunction4(req: https_fn.Request) -> https_fn.Response:
-    bucket = storage.bucket()
+classifier_model = None
+deepdanbooru_model = None
 
-    model_file = bucket.blob('models/sklearn-multiclass-linear-svc.joblib')
+def get_classifier_model():
+    bucket = storage.bucket('danbooru-ml-classifier')
+    model_file = bucket.blob('classifier/sklearn-multiclass-linear-svc.joblib')
     model_bytes_io = BytesIO()
     model_file.download_to_file(model_bytes_io)
     model_bytes_io.seek(0)
-    clf = load(model_bytes_io)
-    print(clf)
 
-    deepdanbooru_model = pretrained_resnet50_model()
-    print(deepdanbooru_model)
+    model = load(model_bytes_io)
 
-    return https_fn.Response(f"Hello world")
+    return model
+
+def get_deepdanbooru_model():
+    bucket = storage.bucket('danbooru-ml-classifier')
+    model_file = bucket.blob('deepdanbooru/0.1/resnet50-13306192.pth')
+    model_bytes_io = BytesIO()
+    model_file.download_to_file(model_bytes_io)
+    model_bytes_io.seek(0)
+
+    model = _resnet(models.resnet50, 6000)
+    model.load_state_dict(torch.load(model_bytes_io, map_location=torch.device('cpu')))
+
+    return model
+
+@https_fn.on_request(memory=options.MemoryOption.GB_1, timeout_sec=600)
+def executeTestFunction4(req: https_fn.Request) -> https_fn.Response:
+    global classifier_model, deepdanbooru_model
+
+    if deepdanbooru_model is None:
+        deepdanbooru_model = get_deepdanbooru_model()
+        print('DeepDanbooru model loaded')
+        print(deepdanbooru_model)
+    else:
+        print('DeepDanbooru model already loaded')
+
+    if classifier_model is None:
+        classifier_model = get_classifier_model()
+        print('Classifier model loaded')
+        print(classifier_model)
+    else:
+        print('Classifier model already loaded')
+
+    bucket = storage.bucket('danbooru-ml-classifier-images')
+    image_file = bucket.blob('danbooru/5744585.png')
+    image_bytes_io = BytesIO()
+    image_file.download_to_file(image_bytes_io)
+    image_bytes_io.seek(0)
+
+    image = Image.open(image_bytes_io)
+    try:
+        if image.mode != 'RGB':
+            image = image.convert('RGB')
+    except Exception as e:
+        print('Image conversion failed')
+        print(e)
+    tags = get_raw_tags(deepdanbooru_model, image)
+
+    return https_fn.Response(json.dumps(tags.tolist()), mimetype='application/json')
---
+++ worker/tagger.py
@@ -0,0 +1,35 @@
+import torch
+from torchvision import transforms
+import json
+from urllib.request import urlopen
+
+class_names = None
+
+def get_raw_tags(model, input_image):
+    global class_names
+
+    if class_names is None:
+        print('Loading class names...')
+        with urlopen("https://github.com/RF5/danbooru-pretrained/raw/master/config/class_names_6000.json") as url:
+            class_names = json.loads(url.read().decode())
+        print('Done loading class names')
+    else:
+        print('Class names already loaded')
+
+    preprocess = transforms.Compose([
+        transforms.Resize(360),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.7137, 0.6628, 0.6519], std=[0.2970, 0.3017, 0.2979]),
+    ])
+
+    input_tensor = preprocess(input_image)
+    input_batch = input_tensor.unsqueeze(0)
+
+    model.eval()
+
+    with torch.no_grad():
+        output = model(input_batch)
+
+    probs = torch.sigmoid(output[0]).cpu()
+
+    return probs
