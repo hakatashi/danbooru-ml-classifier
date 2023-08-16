bb1984b6112461d96e49468231511a7f28f3e43b
Koki Takahashi <hakatasiloving@gmail.com>
2023-08-16 12:48:43 +0000

    Implement getTopImages function

---
+++ publisher/src/api.ts
@@ -0,0 +1,39 @@
+import {getFirestore} from 'firebase-admin/firestore';
+import {getStorage} from 'firebase-admin/storage';
+import {https} from 'firebase-functions/v2';
+
+export const getTopImages = https.onRequest(async (req, res) => {
+	const {date, model: rawModel = '', category: rawCategory = ''} = req.query;
+	const model = rawModel.toString();
+	const category = rawCategory.toString();
+	if (!model.match(/^[0-9a-z_]+$/i) || !category.match(/^[0-9a-z_]+$/i)) {
+		res.status(400).send('Invalid model or category');
+		return;
+	}
+
+	const db = getFirestore();
+	const result = await db.collection('images')
+		.where('date', '==', date)
+		.orderBy(`inferences.${model}.${category}`, 'desc')
+		.limit(100)
+		.get();
+
+	const storage = getStorage();
+	const bucket = storage.bucket('danbooru-ml-classifier-images');
+
+	const images = result.docs.map(async (doc) => {
+		const data = doc.data();
+		const [url] = await bucket.file(data.key).getSignedUrl({
+			version: 'v4',
+			action: 'read',
+			expires: Date.now() + 15 * 60 * 1000,
+		});
+		return {
+			...data,
+			url,
+			score: data.inferences[model][category],
+		};
+	});
+
+	res.json(await Promise.all(images));
+});
--- publisher/src/index.ts
+++ publisher/src/index.ts
@@ -2,5 +2,6 @@ import firebase from 'firebase-admin';
 
 firebase.initializeApp();
 
+export * from './api';
 export * from './pixiv';
 export * from './danbooru';
