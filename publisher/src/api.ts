1b630449a9cdf02c606d9f4fcb778f6c95c82575
Koki Takahashi <hakatasiloving@gmail.com>
2023-08-16 19:27:24 +0000

    Populate size information for getTopImages

--- publisher/src/api.ts
+++ publisher/src/api.ts
@@ -1,12 +1,17 @@
 import {getFirestore} from 'firebase-admin/firestore';
 import {getStorage} from 'firebase-admin/storage';
+import {info} from 'firebase-functions/logger';
 import {defineSecret} from 'firebase-functions/params';
 import {https} from 'firebase-functions/v2';
+import {chunk} from 'lodash';
 
 const hakatashiApiKey = defineSecret('HAKATASHI_API_KEY');
 
 export const getTopImages = https.onRequest(
-	{cors: ['localhost', 'archive.hakatashi.com']},
+	{
+		cors: true,
+		secrets: [hakatashiApiKey],
+	},
 	async (req, res) => {
 		const {date, model: rawModel = '', category: rawCategory = '', apikey: rawApikey = ''} = req.query;
 		if (rawApikey.toString() !== hakatashiApiKey.value()) {
@@ -28,11 +33,72 @@ export const getTopImages = https.onRequest(
 			.limit(100)
 			.get();
 
+		const pixivInfos = new Map<number, {width: number, height: number}>();
+		const pixivArtworkIds = result.docs
+			.filter((doc) => doc.data().type === 'pixiv')
+			.map((doc) => doc.data().artworkId);
+		info(`Fetching ${pixivArtworkIds.length} pixiv artworks`);
+		if (pixivArtworkIds.length > 0) {
+			for (const artworkIds of chunk(pixivArtworkIds, 30)) {
+				const pixivRankings = await db.collection('pixivRanking')
+					.where('artwork.illust_id', 'in', artworkIds)
+					.get();
+				for (const doc of pixivRankings.docs) {
+					const data = doc.data();
+					if (data?.artwork?.illust_id) {
+						pixivInfos.set(data.artwork.illust_id, {
+							width: data.artwork.width,
+							height: data.artwork.height,
+						});
+					}
+				}
+			}
+		}
+
+		const danbooruInfos = new Map<number, {width: number, height: number}>();
+		const danbooruPostIds = result.docs
+			.filter((doc) => doc.data().type === 'danbooru')
+			.map((doc) => doc.data().postId);
+		info(`Fetching ${danbooruPostIds.length} danbooru posts`);
+		if (danbooruPostIds.length > 0) {
+			for (const postIds of chunk(danbooruPostIds, 30)) {
+				const danbooruRankings = await db.collection('danbooruRanking')
+					.where('post.id', 'in', postIds)
+					.get();
+				for (const doc of danbooruRankings.docs) {
+					const data = doc.data();
+					if (data?.post?.id) {
+						danbooruInfos.set(data.post.id, {
+							width: data.post.image_width,
+							height: data.post.image_height,
+						});
+					}
+				}
+			}
+		}
+
 		const storage = getStorage();
 		const bucket = storage.bucket('danbooru-ml-classifier-images');
 
 		const images = result.docs.map(async (doc) => {
 			const data = doc.data();
+
+			let width = 100;
+			let height = 100;
+			if (data.type === 'pixiv' && data.page === 0) {
+				const pixivInfo = pixivInfos.get(data.artworkId);
+				if (pixivInfo) {
+					width = pixivInfo.width;
+					height = pixivInfo.height;
+				}
+			} else if (data.type === 'danbooru') {
+				const danbooruInfo = danbooruInfos.get(data.postId);
+				if (danbooruInfo) {
+					width = danbooruInfo.width;
+					height = danbooruInfo.height;
+				}
+			}
+
 			const [url] = await bucket.file(data.key).getSignedUrl({
 				version: 'v4',
 				action: 'read',
@@ -40,6 +106,8 @@ export const getTopImages = https.onRequest(
 			});
 			return {
 				...data,
+				width,
+				height,
 				url,
 				score: data.inferences[model][category],
 			};
